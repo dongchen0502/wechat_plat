c064bafe96b403ba0c6a300b6fe19eb117960732
dongchen <dongchen.iyunxiao.com>
2016-04-03 22:27:17 +0800

    demo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '旅游WIFI' });
+});
+
+router.get('/api/auth', function(req, res, next){
+
+});
+
+router.post('/api/msg', function(req, res, next){
+
 });
 
 module.exports = router;
